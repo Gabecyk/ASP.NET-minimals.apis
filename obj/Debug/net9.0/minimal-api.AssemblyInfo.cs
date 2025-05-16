566c4ff7af72a545d5e920d8ef6f6305436b27c2
Gabriel Balduíno <gabrielbalduino29@gmail.com>
2025-05-15 23:22:07 -0300

    mysql

--- obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2cc457214ea5156fde51558cf3785075dec07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ea990654007426183358ac91d7b9163179a98c")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
